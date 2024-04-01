6bb0abede11e4b37a957c058dbbf17f61e4aff4a
Zeynep Tarcan <zeyneptarcan08@gmail.com>
2024-04-01 22:36:26 +0100

    commit

---
+++ import os.py
@@ -0,0 +1,13 @@
+import os 
+from random import randint
+
+for i in range(1, 265):
+    d = str(i) + ' days ago'
+
+    for j in range(0,randint(1,10)):
+        with open('file.txt','a') as file: 
+            file.write(d + '\n')
+        os.system('git add .')
+        os.system('git commit --date="'+ d + '" -m "commit "')
+
+os.system('git push -u origin main')
