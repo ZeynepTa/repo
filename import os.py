e8c947cdc314d0a7053155a2b085f431312df0bb
Zeynep Tarcan <zeyneptarcan08@gmail.com>
2024-04-02 00:01:26 +0100

    commit

--- import os.py
+++ import os.py
@@ -1,10 +1,10 @@
 import os 
 from random import randint
 
-for i in range(1, 265):
+for i in range(1, 400):
     d = str(i) + ' days ago'
 
-    for j in range(0,randint(1,10)):
+    for j in range(0,randint(1,3)):
         with open('file.txt','a') as file: 
             file.write(d + '\n')
         os.system('git add .')
